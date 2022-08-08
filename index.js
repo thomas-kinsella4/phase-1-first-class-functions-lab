1e5c2c7fd599b109ab3d314d0ffbdcc9c704f85e
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-08-08 18:41:28 -0400

    commit tree 6506387168f68fb519ef28816cfee26188dad446

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-// Code your solution in this file!
+// // Code your solution in this file!
+
+// const returnFirstTwoDrivers = function(array) {
+//     return array.slice(0,2);
+// };
+
+// const returnLastTwoDrivers = function(array) {
+//     return array.slice(array.length - 2);
+// }
+
+// const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// const createFareMultiplier = function (num) {
+//     return function (fare) {
+//         return fare * num;
+//     }
+// }
+
+
+// const price = createFareMultiplier(2);
+// function fareDoubler (fare) {
+//     return price(fare);
+// }
+
+// const price2 = createFareMultiplier(3);
+// function fareTripler (fare) {
+//     return price2(fare);
+// };
+
+// function selectDifferentDrivers(arrayOfDrivers, ourFunc) {
+//     if (ourFunc === returnFirstTwoDrivers) {
+//         return returnFirstTwoDrivers(arrayOfDrivers);
+//     } else if (ourFunc === returnLastTwoDrivers) {
+//         return returnLastTwoDrivers(arrayOfDrivers);
+//     }
+// }
+
+
+
+
+const returnFirstTwoDrivers = function(array) {
+    return array.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(array) {
+    return array.slice(array.length - 2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fare) {
+    return function(num) {
+        return num * fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(array, func) {
+    if (func === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(array)
+    } 
+
+    if (func === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(array)
+    }
+}
+
